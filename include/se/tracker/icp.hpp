daaba696325b048020b5413b48cce7dff7d15a9d
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2025-03-12 20:34:48 +0200

    Move some ICP functions to a .cpp file

--- include/se/tracker/icp.hpp
+++ include/se/tracker/icp.hpp
@@ -30,46 +30,20 @@ struct Data {
 
 
 
-static inline Eigen::Matrix<float, 6, 6> makeJTJ(const Eigen::Matrix<float, 1, 21>& v)
-{
-    Eigen::Matrix<float, 6, 6> C = Eigen::Matrix<float, 6, 6>::Zero();
-    C.row(0) = v.segment(0, 6);
-    C.row(1).segment(1, 5) = v.segment(6, 5);
-    C.row(2).segment(2, 4) = v.segment(11, 4);
-    C.row(3).segment(3, 3) = v.segment(15, 3);
-    C.row(4).segment(4, 2) = v.segment(18, 2);
-    C(5, 5) = v(20);
+Eigen::Matrix<float, 6, 6> makeJTJ(const Eigen::Matrix<float, 1, 21>& v);
 
-    for (int r = 1; r < 6; ++r)
-        for (int c = 0; c < r; ++c)
-            C(r, c) = C(c, r);
-    return C;
-}
+Eigen::Matrix<float, 6, 1> solve(const Eigen::Matrix<float, 1, 27>& vals);
 
+void newReduce(const int block_idx,
+               float* output_data,
+               const Eigen::Vector2i& output_res,
+               Data* J_data,
+               const Eigen::Vector2i& J_res);
 
-
-static inline Eigen::Matrix<float, 6, 1> solve(const Eigen::Matrix<float, 1, 27>& vals)
-{
-    const Eigen::Matrix<float, 6, 1> b = vals.segment(0, 6);
-    const Eigen::Matrix<float, 6, 6> C = makeJTJ(vals.segment(6, 21));
-    Eigen::LLT<Eigen::Matrix<float, 6, 6>> llt;
-    llt.compute(C);
-    Eigen::Matrix<float, 6, 1> res = llt.solve(b);
-    return llt.info() == Eigen::Success ? res : Eigen::Matrix<float, 6, 1>::Zero();
-}
-
-
-
-static inline void newReduce(const int block_idx,
-                             float* output_data,
-                             const Eigen::Vector2i& output_res,
-                             Data* J_data,
-                             const Eigen::Vector2i& J_res);
-
-static inline void reduceKernel(float* output_data,
-                                const Eigen::Vector2i& output_res,
-                                Data* J_data,
-                                const Eigen::Vector2i& J_res);
+void reduceKernel(float* output_data,
+                  const Eigen::Vector2i& output_res,
+                  Data* J_data,
+                  const Eigen::Vector2i& J_res);
 
 /**
  * ProjectF is functor with the following declaration, returning whether the projection of point_S
@@ -90,15 +64,15 @@ void trackKernel(Data* output_data,
                  const float dist_threshold,
                  const float normal_threshold);
 
-static inline bool updatePoseKernel(Eigen::Isometry3f& T_WS,
-                                    const float* reduction_output_data,
-                                    const float icp_threshold);
+bool updatePoseKernel(Eigen::Isometry3f& T_WS,
+                      const float* reduction_output_data,
+                      const float icp_threshold);
 
-static inline bool checkPoseKernel(Eigen::Isometry3f& T_WS,
-                                   Eigen::Isometry3f& previous_T_WS,
-                                   const float* reduction_output_data,
-                                   const Eigen::Vector2i& reduction_output_res,
-                                   const float track_threshold);
+bool checkPoseKernel(Eigen::Isometry3f& T_WS,
+                     Eigen::Isometry3f& previous_T_WS,
+                     const float* reduction_output_data,
+                     const Eigen::Vector2i& reduction_output_res,
+                     const float track_threshold);
 
 } // namespace icp
 } // namespace se
