585ea2389bb2ba91ecd04a1ec2e40030626bc34b
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2025-10-23 16:44:54 +0300

    Fix missing const
    The sample offset into a voxel shouldn't be configurable at runtime.

--- include/se/map/utils/setup_util.hpp
+++ include/se/map/utils/setup_util.hpp
@@ -12,7 +12,7 @@
 
 namespace se {
 
-static inline Eigen::Vector3f sample_offset_frac = Eigen::Vector3f::Constant(0.5f);
+static inline const Eigen::Vector3f sample_offset_frac = Eigen::Vector3f::Constant(0.5f);
 
 // Representation enums
 enum class Field { TSDF, Occupancy };
