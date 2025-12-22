1fd8cbea844dd56255e5b011c7a5999a039481a2
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2025-12-22 14:21:02 +0200

    Add HTML output to scripts/scale-colours.sh

--- scripts/scale-colours.sh
+++ scripts/scale-colours.sh
@@ -1,19 +1,37 @@
 #!/bin/sh
 # SPDX-FileCopyrightText: 2024 Smart Robotics Lab, Imperial College London, Technical University of Munich
-# SPDX-FileCopyrightText: 2024 Sotiris Papatheodorou
+# SPDX-FileCopyrightText: 2024-2025 Sotiris Papatheodorou
 # SPDX-License-Identifier: BSD-3-Clause
 
-# Use ANSI escape sequences to print the supereight2 scale colours on the
-# terminal by parsing the source code. Assumes a reasonable formatting of the
-# code.
+# Show the supereight2 scale colours by parsing the source code. Assumes a
+# reasonable formatting of the code.
+#
+# Usage:
+# scale-colours.sh
+#   Show the scale colours on a terminal using ANSI escape sequences.
+# scale-colours.sh html
+#   Show the scale colours as an HTML table on standard output.
 
-awk '
+awk -v html="${1:-0}" '
+BEGIN {
+	if (html)
+		print "<table>\n<tr><th>Scale</th><th>Colour</th></tr>"
+}
+
 /^namespace colours \{$/, /^}( \/\/ namespace colours)?$/ {
 	if ($0 ~ "\\{ *[0-9]+, *[0-9]+, *[0-9]+ *}") {
-		sub("^.*\\{", "\033[48;2;")
-		gsub(", *", ";")
-		sub("}.*$", "m          \033[m scale " scale++)
-		print
+		sub("^.*\\{", "")
+		sub("}.*$", "")
+		gsub(", *", " ")
+		if (html)
+			printf "<tr><td>%d</td><td style=\"color:#%x%x%x\">████</td></tr>\n", scale++, $1, $2, $3
+		else
+			printf "\033[48;2;%d;%d;%dm        \033[m scale %d\n", $1, $2, $3, scale++
 	}
+}
+
+END {
+	if (html)
+		print "</table>"
 }
 ' "$(dirname "$0")/../include/se/common/colour_utils.hpp"
