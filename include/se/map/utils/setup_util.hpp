5a95cb31f09bdcad6a8216c8bfd44913af53c3f0
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2025-12-22 12:49:00 +0200

    Improve setup_util.hpp documentation

--- include/se/map/utils/setup_util.hpp
+++ include/se/map/utils/setup_util.hpp
@@ -1,7 +1,7 @@
 /*
  * SPDX-FileCopyrightText: 2021 Smart Robotics Lab, Imperial College London, Technical University of Munich
  * SPDX-FileCopyrightText: 2021 Nils Funk
- * SPDX-FileCopyrightText: 2021 Sotiris Papatheodorou
+ * SPDX-FileCopyrightText: 2021-2025 Sotiris Papatheodorou
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
@@ -17,19 +17,45 @@ namespace se {
  */
 static inline const Eigen::Vector3f g_sample_offset = Eigen::Vector3f::Constant(0.5f);
 
-// Representation enums
-enum class Field { TSDF, Occupancy };
+
+
+/** The type of field stored in the voxels. */
+enum class Field {
+    /** Truncated signed distance function. */
+    TSDF,
+    /** Occupancy probability expressed in log-odds. */
+    Occupancy,
+};
+
+/** Whether the voxels contain colour data of type se::colour_t. */
 enum class Colour { Off, On };
+
+/** Whether the voxels contain identifier data of type se::id_t. */
 enum class Id { Off, On };
 
-// Other enums
-enum class Res { Single, Multi };
-enum class Safe { On = true, Off = false }; // Switch between Safe and Sorry
 
-/**
- *  \brief The enum classes to define the sorting templates
- */
-enum class Sort { SmallToLarge, LargeToSmall };
+
+/** Distinguish between single- and multi-resolution se::Field::TSDF representations. Only
+ * se::Res::Multi is valid for se::Field::Occupancy. */
+enum class Res {
+    /** A classic TSDF representation. Stores data only in the octree leaves. */
+    Single,
+    /** An adaptive-resolution TSDF representation. Stores data at various scales (see se::Scale) of
+     * an se::Block.
+     */
+    Multi,
+};
+
+/** Whether to enable certain safety and bounds checks. */
+enum class Safe { On = true, Off = false };
+
+/** The sort order. */
+enum class Sort {
+    /** Ascending. */
+    SmallToLarge,
+    /** Descending. */
+    LargeToSmall,
+};
 
 } // namespace se
 
