d7e62e224bd572624679e0347e88dbba377820cf
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2026-05-18 18:52:29 +0300

    Reoder test in TSDF raycast
    It makes the interval easier to parse at a glance.

--- include/se/map/impl/raycaster_impl.hpp
+++ include/se/map/impl/raycaster_impl.hpp
@@ -501,7 +501,7 @@ raycast(MapT& map,
         }
 
         f_tt = get_field(data);
-        if (f_tt <= 0.1f && f_tt >= -0.5f) {
+        if (-0.5f <= f_tt && f_tt <= 0.1f) {
             const std::optional<field_t> field_value = [&]() {
                 if constexpr (MapT::res_ == Res::Single) {
                     return map.interpField(point_W);
